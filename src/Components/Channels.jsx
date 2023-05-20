ea41c48a1c2bf9f75189930125d6cc7cfa0e77ae
malay44 <malay.p1@ahduni.edu.in>
2023-05-20 20:07:04 +0530

    error commit

--- src/Components/Channels.jsx
+++ src/Components/Channels.jsx
@@ -3,7 +3,9 @@ import { useState } from "react";
 import * as RiIConn from "react-icons/ri";
 const names = ["Fp1", "Fp2", "F3", "F4", "C3", "C4", "P3", "T3", "T4", "Cz"];
 
-export default function Channels({item, selectedOption, handleChangePrimary, handleChangeRefrence,handleChangeBackupPrimary,handleChangeBackupRefrence}) {
+
+
+export default function Channels({item, selectedOption, handleChangePrimary, handleChangeRefrence,handleChangeBackupPrimary,handleChangeBackupRefrence, handle2ndAddBackupChannels}) {
     
     const [showBackupChannels, setShowBackupChannels] = useState(false);
 
@@ -11,102 +13,8 @@ export default function Channels({item, selectedOption, handleChangePrimary, han
         setShowBackupChannels(!showBackupChannels);
     };
 
-    return (
-    <>
-      <Box
-        sx={{
-          height: "100px",
-          backgroundColor: "white",
-          borderRadius: "5px",
-          alignItems: "center",
-          justifyContent: "space-around",
-          display: "grid",
-          gridTemplateColumns: "25% 25% 25% 25%",
-          mt: 2,
-        }}
-      >
-        <Typography
-          variant="p"
-          sx={{
-            color: "#0F0F0F",
-            fontSize: "18px",
-            fontWeight: "400",
-            textTransform: "capitalize",
-            textAlign: "center",
-          }}
-        >
-          {item}
-        </Typography>
-
-        <select
-          style={{
-            padding: "12px",
-            borderRadius: "5px",
-            border: "1px solid #E5E5E5",
-            width: "70%",
-            fontSize: "16px",
-            fontWeight: "400",
-            color: "#959595",
-          }}
-          required={true}
-          id={item}
-          className="md:mx-0 mx-5 border-2 p-2 rounded-md mt-8"
-          type="string"
-          value={selectedOption[item]?.primary || "null"}
-          onChange={(e) => {
-            handleChangePrimary(e);
-          }}
-        >
-          <option value="null">Select Channel 323</option>
-          {names.map((name) => {
-            return <option value={name}>{name}</option>;
-          })}
-        </select>
-
-        <select
-          style={{
-            padding: "12px",
-            borderRadius: "5px",
-            border: "1px solid #E5E5E5",
-            width: "70%",
-            fontSize: "16px",
-            fontWeight: "400",
-            color: "#959595",
-          }}
-          required={true}
-          id={item}
-          type="string"
-          value={selectedOption[item]?.refrence || "null"}
-          onChange={handleChangeRefrence}
-        >
-          <option value="null">Select Channel 4</option>
-          {names.map((name) => {
-            return <option value={name}>{name}</option>;
-          })}
-        </select>
-
-        <Button
-          onClick={handleAddBackupChannels}
-          variant="text"
-          component="span"
-          sx={{ color: "#959595" }}
-        >
-          <Typography
-            variant="p"
-            sx={{
-              color: "#2F7EC7",
-              fontSize: "14px",
-              fontWeight: "500",
-              textTransform: "capitalize",
-            }}
-          >
-            {" "}
-            + Add Backup Channles
-          </Typography>
-        </Button>
-      </Box>
-
-      <Collapse in={showBackupChannels} timeout="auto" unmountOnExit>
+    const BackupChannel = ({BackupID}) => {
+      return (
         <Box
           sx={{
             height: "100px",
@@ -118,7 +26,7 @@ export default function Channels({item, selectedOption, handleChangePrimary, han
             gridTemplateColumns: "25% 25% 25% 25%",
           }}
         >
-          <div style={{ color: "#F6F6F6" }}>df</div>
+          <div style={{ color: "#F6F6F6" }}>elo</div>
           <select
             style={{
               padding: "12px",
@@ -133,7 +41,7 @@ export default function Channels({item, selectedOption, handleChangePrimary, han
             id={item}
             className="md:mx-0 mx-5 border-2 p-2 rounded-md mt-8"
             type="string"
-            value={selectedOption[item]?.backupPrimary || "null"}
+            value={selectedOption[item.split("-")[1] ][item]?.backupPrimary || "null"}
             onChange={handleChangeBackupPrimary}
           >
             <option value="null">Select Channel</option>
@@ -155,7 +63,7 @@ export default function Channels({item, selectedOption, handleChangePrimary, han
             required={true}
             id={item}
             type="string"
-            value={selectedOption[item]?.backupRefrence || "null"}
+            value={selectedOption[item.split("-")[1] - 1][item]?.backupRefrence || "null"}
             onChange={handleChangeBackupRefrence}
           >
             <option value="null">Select Channel</option>
@@ -182,7 +90,167 @@ export default function Channels({item, selectedOption, handleChangePrimary, han
             </Typography>
           </Button>
         </Box>
-      </Collapse>
-    </>
-  );
+      );
+    };
+
+    return (
+      <>
+        <Box
+          sx={{
+            height: "100px",
+            backgroundColor: "white",
+            borderRadius: "5px",
+            alignItems: "center",
+            justifyContent: "space-around",
+            display: "grid",
+            gridTemplateColumns: "25% 25% 25% 25%",
+            mt: 2,
+          }}
+        >
+          <Typography
+            variant="p"
+            sx={{
+              color: "#0F0F0F",
+              fontSize: "18px",
+              fontWeight: "400",
+              textTransform: "capitalize",
+              textAlign: "center",
+            }}
+          >
+            {item}
+          </Typography>
+
+          <select
+            style={{
+              padding: "12px",
+              borderRadius: "5px",
+              border: "1px solid #E5E5E5",
+              width: "70%",
+              fontSize: "16px",
+              fontWeight: "400",
+              color: "#959595",
+            }}
+            required={true}
+            id={item}
+            className="md:mx-0 mx-5 border-2 p-2 rounded-md mt-8"
+            type="string"
+            value={selectedOption[item.split("-")[1] - 1][item]?.primary || "null"}
+            onChange={(e) => {
+              handleChangePrimary(e);
+            }}
+          >
+            <option value="null">Select Channel</option>
+            {names.map((name) => {
+              return <option value={name}>{name}</option>;
+            })}
+          </select>
+
+          <select
+            style={{
+              padding: "12px",
+              borderRadius: "5px",
+              border: "1px solid #E5E5E5",
+              width: "70%",
+              fontSize: "16px",
+              fontWeight: "400",
+              color: "#959595",
+            }}
+            required={true}
+            id={item}
+            type="string"
+            value={selectedOption[item.split("-")[1] - 1][item]?.refrence || "null"}
+            onChange={handleChangeRefrence}
+          >
+            <option value="null">Select Channel</option>
+            {names.map((name) => {
+              return <option value={name}>{name}</option>;
+            })}
+          </select>
+
+          <Button
+            onClick={handleAddBackupChannels}
+            variant={
+              showBackupChannels
+                ? "text"
+                : selectedOption[item.split("-")[1] - 1][item]?.backupPrimary ||
+                  selectedOption[item.split("-")[1] - 1][item]?.backupRefrence
+                ? "text"
+                : "outlined"
+            }
+            component="span"
+            sx={{
+              color: "#2F7EC7",
+              width: "70%",
+              height: "60px",
+              borderRadius: "7px",
+            }}
+          >
+            <Typography
+              variant="p"
+              sx={{
+                color: "#2F7EC7",
+                fontSize: "14px",
+                fontWeight: "500",
+                textTransform: "capitalize",
+              }}
+            >
+              {showBackupChannels
+                ? "Hide backup channels"
+                : selectedOption[item.split("-")[1] - 1][item]?.backupPrimary ||
+                  selectedOption[item.split("-")[1] - 1][item]?.backupRefrence
+                ? "View backup channels"
+                : "+ Add Backup Channles"}
+            </Typography>
+          </Button>
+        </Box>
+
+        <Collapse in={showBackupChannels} timeout="auto" unmountOnExit>
+            <BackupChannel BackupID={0}/>
+          {(selectedOption[item.split("-")[1] - 1][item]?.backupPrimary || [] ).map((item, index) => {
+            return <BackupChannel BackupID={index+1} />;
+          })}
+          <Box
+            sx={{
+              height: "10px",
+              backgroundColor: "#F6F6F6",
+              paddingBottom: "30px",
+              borderRadius: "5px",
+              alignItems: "center",
+              justifyContent: "center",
+              display: "grid",
+              gridTemplateColumns: "25% 25% 25% 25%",
+            }}
+          >
+            <div style={{ color: "#F6F6F6" }}>elo</div>
+            <Button
+              id={item}
+              variant="text"
+              component="span"
+              sx={{
+                color: "#959595",
+                width: "65%",
+                padding: 0,
+                height: "30px",
+                borderRadius: "7px",
+              }}
+              onClick={handle2ndAddBackupChannels}
+            >
+              <Typography
+                variant="p"
+                sx={{
+                  color: "#2CA9E3",
+                  fontSize: "14px",
+                  fontWeight: "500",
+                  textTransform: "capitalize",
+                }}
+              >
+                + Add Backup Channles
+              </Typography>
+            </Button>
+            <div style={{ color: "#F6F6F6" }}>elo</div>
+            <div style={{ color: "#F6F6F6" }}>elo</div>
+          </Box>
+        </Collapse>
+      </>
+    );
 }
--- src/Components/MapChannels.jsx
+++ src/Components/MapChannels.jsx
@@ -5,79 +5,104 @@ import { Box, Typography } from "@mui/material";
 import sampleData from "../data/sample.json";
 import Channels from "./Channels";
 
-
 const MapChannels = () => {
   const dispatch = useDispatch();
   const montage = useSelector((state) => state.montage.selectedOption);
   const [selectedOption, setSelectedOption] = useState({});
+    
 
   useEffect(() => {
-    console.log("montage", montage);
-    setSelectedOption(montage);
-  }, []);
+      setSelectedOption(montage);
+  }, [])
 
-    useEffect(() => {
-        dispatch(setOptions(selectedOption));
-        console.log("selectedOption", selectedOption);
-    }, [dispatch, selectedOption]);
+  useEffect(() => {
+    dispatch(setOptions(selectedOption));
+    // console.log("selectedOption", selectedOption);
+  }, [dispatch, selectedOption]);
+
+  console.log("----------------------------------------------------");
+  console.log("selectedOption", selectedOption);
+  console.log("----------------------------------------------------");
+  console.log("montage", montage);
 
   const handleChangePrimary = (event) => {
     const channelName = event.target.id;
+    const channelIndex = channelName.split("-")[1] - 1;
     const newValue = event.target.value;
     setSelectedOption((prevState) => {
       return {
         ...prevState,
-        [channelName]: {
-          ...prevState[channelName],
-          primary: newValue,
+        [channelIndex]: {
+          ...prevState[channelIndex],
+          [channelName]: {
+            ...prevState[channelIndex][channelName],
+            primary: newValue,
+          },
         },
       };
     });
   };
 
+  //similarly for refrence
+
   const handleChangeRefrence = (event) => {
     const channelName = event.target.id;
+    const channelIndex = channelName.split("-")[1] - 1;
     const newValue = event.target.value;
     setSelectedOption((prevState) => {
       return {
         ...prevState,
-        [channelName]: {
-          ...prevState[channelName],
-          refrence: newValue,
+        [channelIndex]: {
+          ...prevState[channelIndex],
+          [channelName]: {
+            ...prevState[channelIndex][channelName],
+            refrence: newValue,
+          },
         },
       };
+    });
+  };
+
+  const handle2ndAddBackupChannels = (event) => {
+    const channelName = event.target.id;
+    console.log("channelName", channelName);
+    setSelectedOption((prevState) => {
+      const newState = { ...prevState };
+      console.log("newState", newState);
+      // newState[channelName].backupPrimary.push(null);
+      return newState;
     });
   };
 
   //same for backup primary and backup refrence
 
-    const handleChangeBackupPrimary = (event) => {
-        const channelName = event.target.id;
-        const newValue = event.target.value;
-        setSelectedOption((prevState) => {
-            return {
-                ...prevState,
-                [channelName]: {
-                    ...prevState[channelName],
-                    backupPrimary: newValue,
-                },
-            };
-        });
-    };
+  const handleChangeBackupPrimary = (event) => {
+    const channelName = event.target.id;
+    const newValue = event.target.value;
+    setSelectedOption((prevState) => {
+      return {
+        ...prevState,
+        [channelName]: {
+          ...prevState[channelName],
+          backupPrimary: newValue,
+        },
+      };
+    });
+  };
 
-    const handleChangeBackupRefrence = (event) => {
-        const channelName = event.target.id;
-        const newValue = event.target.value;
-        setSelectedOption((prevState) => {
-            return {
-                ...prevState,
-                [channelName]: {
-                    ...prevState[channelName],
-                    backupRefrence: newValue,
-                },
-            };
-        });
-    };
+  const handleChangeBackupRefrence = (event) => {
+    const channelName = event.target.id;
+    const newValue = event.target.value;
+    setSelectedOption((prevState) => {
+      return {
+        ...prevState,
+        [channelName]: {
+          ...prevState[channelName],
+          backupRefrence: newValue,
+        },
+      };
+    });
+  };
 
   return (
     <div>
@@ -132,14 +157,15 @@ const MapChannels = () => {
 
       {sampleData.channels.map((item) => {
         return (
-            <Channels 
+          <Channels
             item={item}
             selectedOption={selectedOption}
             handleChangePrimary={handleChangePrimary}
             handleChangeRefrence={handleChangeRefrence}
             handleChangeBackupPrimary={handleChangeBackupPrimary}
             handleChangeBackupRefrence={handleChangeBackupRefrence}
-             />
+            handle2ndAddBackupChannels={handle2ndAddBackupChannels}
+          />
         );
       })}
     </div>
